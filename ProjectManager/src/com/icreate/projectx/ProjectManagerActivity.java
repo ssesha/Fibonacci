2d305d30c2f30e15e80dbeac40ba9c22211490a5
abbinayaa92 <abbinayaa92@gmail.com>
2012-05-27 22:36:14 +0800

    module code integrated instant search

--- ProjectManager/src/com/icreate/projectx/ProjectManagerActivity.java
+++ ProjectManager/src/com/icreate/projectx/ProjectManagerActivity.java
@@ -1,12 +1,17 @@
 package com.icreate.projectx;
 
+import com.icreate.projectx.datamodel.ProjectxGlobalState;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ProjectManagerActivity extends Activity {
 
@@ -18,12 +23,61 @@ public class ProjectManagerActivity extends Activity {
 
 		Button loginButton = (Button) findViewById(R.id.loginButton);
 
-		final Context cont = this;
+        WebView wv = (WebView) findViewById(R.id.WebViewLogin);                  
+        //WebSettings webSettings = wv.getSettings();
+        //webSettings.setBuiltInZoomControls(true);
+        wv.getSettings().setJavaScriptEnabled(true); //to enable javascript
+        /* Register a new JavaScript interface called HTMLOUT */
+        wv.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+        
+        wv.setWebViewClient(new WebViewClient() 
+        {               
+        	@Override               
+        	public void onPageFinished(WebView view, String url) 
+        	{     
+        		Log.d("url: " + url, "onPageFinished");
+        		// when login is complete, the url will be login_result.ashx?r=0
+        		if (url.indexOf("/api/login/login_result.ashx") > 0)
+        		{
+        			// When login is successful, there will be a &r=0 in the url. It also means the return data is the token itself.
+        			if (url.indexOf("&r=0") > 0)
+        			{
+        				Log.d("success", "onPageFinished");        				
+        				Log.i("onPageFinished - before loading javascript", "");
+        				view.loadUrl("javascript:window.HTMLOUT.processHTML(document.getElementsByTagName('body')[0].innerHTML);");
+        			}
+        		}
+        		else 
+        		{
+        			Log.d("error","login not complete");
+        		}
+        	}
+        });
+    
+        wv.loadUrl("https://ivle.nus.edu.sg/api/login/?apikey=tlXXFhEsNoTIVTJQruS2o");
+    
+	
+		
+		
+		 /*
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				startActivity(new Intent(cont, homeActivity.class));
 			}
-		});
+		});*/
 	}
+	final Context cont = this;
+	
+	class MyJavaScriptInterface
+    {
+        @SuppressWarnings("unused")
+        public void processHTML(String html)
+        {
+        	Log.d("onPageFinished ", "inside javascript interface");
+        	 ProjectxGlobalState Token = (ProjectxGlobalState)getApplication();
+        	 Token.setAuthToken(html);
+        	 startActivity(new Intent(cont, homeActivity.class));
+        }
+    }
 
 }
--- ProjectManager/src/com/icreate/projectx/newProjectActivity.java
+++ ProjectManager/src/com/icreate/projectx/newProjectActivity.java
@@ -3,18 +3,37 @@ package com.icreate.projectx;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.icreate.projectx.datamodel.ProjectxGlobalState;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 public class newProjectActivity extends Activity {
-	private ListView moduleListView,membersListView;
-	private EditText moduleTextBox,membersTextBox;
+	private ListView moduleListView, membersListView;
+	private EditText moduleTextBox, membersTextBox, nameTextBox, aboutTextBox,
+			dueTextBox;
+	private Button loginButton;
 	private List<String> moduleList = new ArrayList<String>();
 	private List<String> studentList = new ArrayList<String>();
 	private ArrayList<String> moduleListFilter = new ArrayList<String>();
@@ -25,31 +44,52 @@ public class newProjectActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.newproject);
-	    getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.logo1);
-	    
-	    moduleListView = (ListView) findViewById(R.id.modulesListView);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.logo1);
+
+		moduleListView = (ListView) findViewById(R.id.modulesListView);
 		moduleTextBox = (EditText) findViewById(R.id.moduleTextBox);
 		membersListView = (ListView) findViewById(R.id.membersListView);
 		membersTextBox = (EditText) findViewById(R.id.membersTextBox);
-		
-		moduleList.add("CS1231");
-		moduleList.add("CS1101");
-		moduleList.add("CS2103");
-		moduleList.add("CS3215");
-		moduleList.add("CS4444");
+		nameTextBox = (EditText) findViewById(R.id.nameTextBox);
+		aboutTextBox = (EditText) findViewById(R.id.aboutTextBox);
+		dueTextBox = (EditText) findViewById(R.id.deadlineTextBox);
+		loginButton = (Button) findViewById(R.id.loginButton);
+		getModuleList();
 		studentList.add("Oinker");
 		studentList.add("Abs");
 		studentList.add("Abbinayaa");
 		studentList.add("oink");
 		studentList.add("oinky");
 		studentList.add("piggy");
-		
-		moduleListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, moduleList));
-		membersListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, studentList));
-		
+
+		moduleListView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, moduleList));
+		membersListView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, studentList));
+
+		moduleListView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> myAdapter, View myView,
+					int myItemInt, long mylng) {
+				String selectedFromList = (String) (moduleListView
+						.getItemAtPosition(myItemInt));
+				moduleTextBox.setText(selectedFromList);
+				moduleListView.setVisibility(View.INVISIBLE);
+			}
+		});
+
+		membersListView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> myAdapter, View myView,
+					int myItemInt, long mylng) {
+				String selectedFromList = (String) (membersListView
+						.getItemAtPosition(myItemInt));
+				membersTextBox.setText(selectedFromList);
+				membersListView.setVisibility(View.INVISIBLE);
+			}
+		});
+
 		moduleTextBox.addTextChangedListener(new TextWatcher() {
 
 			public void afterTextChanged(Editable s) {
@@ -62,26 +102,32 @@ public class newProjectActivity extends Activity {
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
 
+				moduleListView.setVisibility(View.VISIBLE);
 				textLength1 = moduleTextBox.getText().length();
+
+				if (textLength1 == 0)
+					moduleListView.setVisibility(View.INVISIBLE);
 				moduleListFilter.clear();
 				for (int i = 0; i < moduleList.size(); i++) {
 					if (textLength1 <= moduleList.get(i).length()) {
-						if (moduleTextBox.getText()
+						if (moduleTextBox
+								.getText()
 								.toString()
 								.equalsIgnoreCase(
-										(String) moduleList.get(i).subSequence(0,
-												textLength1))) {
+										(String) moduleList.get(i).subSequence(
+												0, textLength1))) {
 							moduleListFilter.add(moduleList.get(i));
 						}
 					}
 				}
 
-				moduleListView.setAdapter(new ArrayAdapter<String>(newProjectActivity.this,
+				moduleListView.setAdapter(new ArrayAdapter<String>(
+						newProjectActivity.this,
 						android.R.layout.simple_list_item_1, moduleListFilter));
 
 			}
 		});
-		
+
 		membersTextBox.addTextChangedListener(new TextWatcher() {
 
 			public void afterTextChanged(Editable s) {
@@ -94,25 +140,86 @@ public class newProjectActivity extends Activity {
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
 
+				membersListView.setVisibility(View.VISIBLE);
 				textLength2 = membersTextBox.getText().length();
+				if (textLength2 == 0)
+					membersListView.setVisibility(View.INVISIBLE);
+
 				studentListFilter.clear();
 				for (int i = 0; i < studentList.size(); i++) {
 					if (textLength2 <= studentList.get(i).length()) {
-						if (membersTextBox.getText()
+						if (membersTextBox
+								.getText()
 								.toString()
 								.equalsIgnoreCase(
-										(String) studentList.get(i).subSequence(0,
-												textLength2))) {
+										(String) studentList.get(i)
+												.subSequence(0, textLength2))) {
 							studentListFilter.add(studentList.get(i));
 						}
 					}
 				}
 
-				membersListView.setAdapter(new ArrayAdapter<String>(newProjectActivity.this,
-						android.R.layout.simple_list_item_1, studentListFilter));
+				membersListView
+						.setAdapter(new ArrayAdapter<String>(
+								newProjectActivity.this,
+								android.R.layout.simple_list_item_1,
+								studentListFilter));
 
 			}
 		});
+
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				JSONObject json1 = new JSONObject();
+				try {
+					json1.put("name", nameTextBox.getText());
+					json1.put("description", aboutTextBox.getText());
+					json1.put("leader", "u0909080@nus.edu.sg");
+					json1.put("moduleCode", moduleTextBox.getText());
+					json1.put("duedate", dueTextBox.getText());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("JSON string"+ json1.toString());
+			}
+		});
+	}
+
+	private void getModuleList() {
+		try {
+			HttpClient client = new DefaultHttpClient();
+			ProjectxGlobalState globalData = (ProjectxGlobalState) getApplication();
+			String getURL = "https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Student?APIKey=tlXXFhEsNoTIVTJQruS2o&AuthToken="
+					+ globalData.getAuthToken()
+					+ "&Duration=0&IncludeAllInfo=false";
+			HttpGet get = new HttpGet(getURL);
+			HttpResponse responseGet = client.execute(get);
+			HttpEntity mResEntityGet = responseGet.getEntity();
+			String content;
+
+			JSONObject json = new JSONObject();
+
+			if (mResEntityGet != null) {
+				content = EntityUtils.toString(mResEntityGet);
+				Log.d("response", content);
+				json = new JSONObject(content);
+				String xyz = json.getString("Results");
+				JSONArray arr = new JSONArray(xyz);
+				// Log.d("json", json.toString(3));
+
+				for (int i = 0; i < arr.length(); i++) {
+					JSONObject obj = arr.getJSONObject(i);
+					String courseid = obj.getString("CourseCode");
+					moduleList.add(courseid);
+					Log.d("result", courseid);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("module-error", "could not get modules");
+		}
+
 	}
 
 }
