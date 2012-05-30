d5c745db5cf4f369541fa74c02ed821b52abeb08
abbinayaa92 <abbinayaa92@gmail.com>
2012-05-31 04:02:19 +0800

    have updated the adding members part and deadline selection.return member names in json string

--- ProjectManager/src/com/icreate/projectx/newProjectActivity.java
+++ ProjectManager/src/com/icreate/projectx/newProjectActivity.java
@@ -16,30 +16,41 @@ import org.json.JSONObject;
 import com.icreate.projectx.datamodel.ProjectxGlobalState;
 
 import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class newProjectActivity extends Activity {
 	private ListView moduleListView, membersListView;
-	private EditText moduleTextBox, membersTextBox, nameTextBox, aboutTextBox,
+	private EditText moduleTextBox, membersTextBox1, nameTextBox, aboutTextBox,
 			dueTextBox;
+	private DatePicker dp1;
 	private Button loginButton;
 	private List<String> moduleList = new ArrayList<String>();
 	private List<String> studentList = new ArrayList<String>();
 	private ArrayList<String> moduleListFilter = new ArrayList<String>();
 	private ArrayList<String> studentListFilter = new ArrayList<String>();
+	private ArrayList<String> members=new ArrayList<String>();
 	int textLength1 = 0;
 	int textLength2 = 0;
+	int index=0;
+	String text;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -52,11 +63,12 @@ public class newProjectActivity extends Activity {
 		moduleListView = (ListView) findViewById(R.id.modulesListView);
 		moduleTextBox = (EditText) findViewById(R.id.moduleTextBox);
 		membersListView = (ListView) findViewById(R.id.membersListView);
-		membersTextBox = (EditText) findViewById(R.id.membersTextBox);
+		membersTextBox1 = (EditText) findViewById(R.id.membersTextBox1);
 		nameTextBox = (EditText) findViewById(R.id.nameTextBox);
 		aboutTextBox = (EditText) findViewById(R.id.aboutTextBox);
 		dueTextBox = (EditText) findViewById(R.id.deadlineTextBox);
 		loginButton = (Button) findViewById(R.id.loginButton);
+		dp1= (DatePicker)findViewById(R.id.datePicker1);
 		getModuleList();
 		studentList.add("Oinker");
 		studentList.add("Abs");
@@ -64,7 +76,10 @@ public class newProjectActivity extends Activity {
 		studentList.add("oink");
 		studentList.add("oinky");
 		studentList.add("piggy");
+		RelativeLayout rl = (RelativeLayout)findViewById(R.id.rlayout);
 
+		
+        
 		moduleListView.setAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, moduleList));
 		membersListView.setAdapter(new ArrayAdapter<String>(this,
@@ -85,11 +100,34 @@ public class newProjectActivity extends Activity {
 					int myItemInt, long mylng) {
 				String selectedFromList = (String) (membersListView
 						.getItemAtPosition(myItemInt));
-				membersTextBox.setText(selectedFromList);
+				//membersTextBox1.setText(selectedFromList+"\n");
+				LinearLayout ll = (LinearLayout)findViewById(R.id.layout1);
+				EditText t = new EditText(newProjectActivity.this);
+				t.setText(selectedFromList);
+				t.setCompoundDrawablesWithIntrinsicBounds(null, null, getResources().getDrawable(R.drawable.formvalidationerror), null);
+				t.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+		        ll.addView(t);
+		        membersTextBox1.setText("");
+		        members.add(selectedFromList);
 				membersListView.setVisibility(View.INVISIBLE);
 			}
 		});
 
+		dueTextBox.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	        	dp1.setVisibility(View.VISIBLE);
+	        }
+
+	    });
+		
+		rl.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				dueTextBox.setText(dp1.getDayOfMonth()+"-"+dp1.getMonth()+"-"+dp1.getYear());
+				dp1.setVisibility(View.INVISIBLE);
+			}
+		});
+		
 		moduleTextBox.addTextChangedListener(new TextWatcher() {
 
 			public void afterTextChanged(Editable s) {
@@ -102,7 +140,7 @@ public class newProjectActivity extends Activity {
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
 
-				moduleListView.setVisibility(View.VISIBLE);
+				
 				textLength1 = moduleTextBox.getText().length();
 
 				if (textLength1 == 0)
@@ -124,11 +162,16 @@ public class newProjectActivity extends Activity {
 				moduleListView.setAdapter(new ArrayAdapter<String>(
 						newProjectActivity.this,
 						android.R.layout.simple_list_item_1, moduleListFilter));
+				
+				if(moduleListView.getCount()>=1 && textLength1>0 )
+				{
+				moduleListView.setVisibility(View.VISIBLE);
+				}
 
 			}
 		});
 
-		membersTextBox.addTextChangedListener(new TextWatcher() {
+		membersTextBox1.addTextChangedListener(new TextWatcher() {
 
 			public void afterTextChanged(Editable s) {
 			}
@@ -139,19 +182,14 @@ public class newProjectActivity extends Activity {
 
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
-
-				membersListView.setVisibility(View.VISIBLE);
-				textLength2 = membersTextBox.getText().length();
+				textLength2 = membersTextBox1.getText().length();
 				if (textLength2 == 0)
 					membersListView.setVisibility(View.INVISIBLE);
 
 				studentListFilter.clear();
 				for (int i = 0; i < studentList.size(); i++) {
 					if (textLength2 <= studentList.get(i).length()) {
-						if (membersTextBox
-								.getText()
-								.toString()
-								.equalsIgnoreCase(
+						if (membersTextBox1.getText().toString().equalsIgnoreCase(
 										(String) studentList.get(i)
 												.subSequence(0, textLength2))) {
 							studentListFilter.add(studentList.get(i));
@@ -164,6 +202,11 @@ public class newProjectActivity extends Activity {
 								newProjectActivity.this,
 								android.R.layout.simple_list_item_1,
 								studentListFilter));
+				
+				if(membersListView.getCount()>=1 && textLength2>0)
+				{
+				membersListView.setVisibility(View.VISIBLE);
+				}
 
 			}
 		});
@@ -177,14 +220,20 @@ public class newProjectActivity extends Activity {
 					json1.put("leader", "u0909080@nus.edu.sg");
 					json1.put("moduleCode", moduleTextBox.getText());
 					json1.put("duedate", dueTextBox.getText());
+					for(int i=0;i<members.size();i++)
+					{
+						json1.put("member"+i, members.get(i));
+					}
+					Log.d("JSON string",json1.toString());
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
 
-				System.out.println("JSON string"+ json1.toString());
+				
 			}
 		});
 	}
+
 
 	private void getModuleList() {
 		try {
