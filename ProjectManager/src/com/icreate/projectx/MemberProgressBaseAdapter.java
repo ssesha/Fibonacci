9f3b62d225b4b4ec682e92cf10bf5f67a3b89cc3
vandhanaa <vandhanaa91@gmail.com>
2012-06-16 21:05:04 +0800

    Project View

---
+++ ProjectManager/src/com/icreate/projectx/MemberProgressBaseAdapter.java
@@ -0,0 +1,15 @@
+package com.icreate.projectx;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.icreate.projectx.datamodel.Task;
+public class MemberProgressBaseAdapter {
+
+}
--- ProjectManager/src/com/icreate/projectx/projectViewActivity.java
+++ ProjectManager/src/com/icreate/projectx/projectViewActivity.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,43 +18,40 @@ public class projectViewActivity extends Activity {
 	private TextView ProjectName;
 	private Button createTask;
 	private int position;
+	private ProjectxGlobalState globalState;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
+	    //requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 	    setContentView(R.layout.projectview);
+	    //getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.logo1);
+	    	    
 		final Context cont = this;
 		final Activity currentActivity = this;
-		
-		//ProjectName=(TextView)findViewById(R.id.projectName);
 				
-		
 		createTask=(Button)findViewById(R.id.createNewTaskButton);
 		
 		Bundle extras = getIntent().getExtras();
-		ProjectxGlobalState global = (ProjectxGlobalState)getApplication();
+		globalState = (ProjectxGlobalState)getApplication();
 		if(extras!=null)
 		{
 			position=extras.getInt("position");
 			System.out.println(position);
 			
-			Toast.makeText(cont, "" + global.getProjectList().getProjects().get(position).getProject_name(),
+			Toast.makeText(cont, "" + globalState.getProjectList().getProjects().get(position).getProject_name(),
 				Toast.LENGTH_LONG).show();
 		}
+		final ListView projectListView = (ListView) findViewById(R.id.memberProgressList);
 		
-		//ProjectName.setText(global.getProjectList().getProjects().get(position).getProject_name());
-		//Log.d("project id", project_id);
-		
+		projectListView.setTextFilterEnabled(true);
 		createTask.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent newTaskIntent = new Intent(cont,
 						newTaskActivity.class);
 					newTaskIntent.putExtra("position",position );
-				startActivity(newTaskIntent);
-				
-			}
-			
+				startActivity(newTaskIntent);				
+			}			
 		});
 	}
 
